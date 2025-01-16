6aed70b04569511fc41b774098eeb126039ba56b
wongyuenying <angelwyy1021@gmail.com>
2025-01-16 17:57:21 +1100

    Update of Dice Class

--- src/model/Dice.java
+++ src/model/Dice.java
@@ -1,5 +1,28 @@
 package model;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import fileLoader.Loader;
+
 public class Dice {
 
+	private Queue<Integer> rolls;
+
+	//Queue<Integer> rollData
+	
+	public Dice(String filepath) {
+		List<Integer> rollData = Loader.loadRolls(filepath);
+		this.rolls = new LinkedList<>(rollData);
+	}
+
+	// Return the rolls number and pop it out of the queue.
+	public int roll() {
+		return rolls.poll();
+	}
+
+	public boolean endOfRolls() {
+		return rolls.isEmpty();
+	}
 }
