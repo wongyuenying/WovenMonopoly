62ab9b3eb0f95103ba187bae02515baf00592ef3
wongyuenying <angelwyy1021@gmail.com>
2025-01-16 13:17:35 +1100

    Update of Player class
    Initialise setter, getter, and basic functions of the class

--- src/model/Player.java
+++ src/model/Player.java
@@ -2,4 +2,60 @@ package model;
 
 public class Player {
 
+	private String name;
+	private int money;
+	private int position;
+	private boolean isBankrupt;
+	
+	public Player(String name, int money) {
+		this.name = name;
+		this.money = money;
+		this.position = 0;
+		this.isBankrupt = false;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getMoney() {
+		return money;
+	}
+	
+	public int getPosition() {
+		return position;
+	}
+	
+	public boolean isBankrupt() {
+		return isBankrupt;
+	}
+	
+	public void move(int steps, int boardSize) {
+		position = (position + steps) % boardSize;
+	}
+	
+	public void payRent(int amount) {
+		money -= amount;
+		checkIsBankrupt();
+	}
+	
+	private void checkIsBankrupt() {
+		if (money < 0) {
+			isBankrupt = true;
+		}
+	}
+	
+	public void receiveRent(int amount) {
+		money += amount;
+	}
+	
 }
+	
