a585cfcc36d2b2df5b3723aba86088686792cde4
wongyuenying <angelwyy1021@gmail.com>
2025-01-16 15:00:56 +1100

    Testing of Loader functions
    try to test and print out the json files

--- src/gamePlay/Main.java
+++ src/gamePlay/Main.java
@@ -1,5 +1,18 @@
 package gamePlay;
 
-public class Main {
+import fileLoader.Loader;
+import java.util.List;
+import java.util.Map;
 
+public class Main {
+	
+	public static void main(String args[]) {
+		
+		List<Map<String, Object>> board = Loader.loadBoard("board.json");
+		List<Integer> rolls = Loader.loadRolls("rolls_1.json");
+		 System.out.println("Board: " + board);
+		 System.out.println("Roll: " + rolls);
+	}
+	
+	
 }
